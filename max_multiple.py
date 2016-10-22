d076f023461d5beb3944b341a37c6884bca82778
shrobins <sethrobinson@gmail.com>
2016-10-22 12:16:04 -0400

    Update max_multiple.py

--- max_multiple.py
+++ max_multiple.py
@@ -1,8 +1,3 @@
-#Given a divisor and a bound, find the largest integer N such that:
-#N is divisible by divisor.
-#N is less than or equal to bound.
-#N is greater than 0.
-
 def maxMultiple(divisor, bound):
     l = []
     for i in range(bound+1): 
