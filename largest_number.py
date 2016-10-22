ce463785de2de6960f25dee6539f1516de40deab
shrobins <sethrobinson@gmail.com>
2016-10-22 11:57:23 -0400

    Create largest_number.py

---
+++ largest_number.py
@@ -0,0 +1,15 @@
+def largestNumber(n): 
+    if n == 1: 
+        return 9
+    elif n == 2: 
+        return 99
+    elif n == 3: 
+        return 999
+    elif n == 4: 
+        return 9999
+    elif n == 5: 
+        return 99999
+    elif n == 6: 
+        return 999999
+    elif n == 7: 
+        return 9999999
