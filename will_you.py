a7b691ed094ac2ca6acf920ad4d3472872fb25b4
shrobins <sethrobinson@gmail.com>
2016-10-22 12:23:39 -0400

    Create will_you.py

---
+++ will_you.py
@@ -0,0 +1,10 @@
+#not passing test failing 
+
+def willYou(young, beautiful, loved):
+    
+    if young != beautiful and loved==True: 
+        return True 
+    elif young==loved:
+        return False 
+    else: 
+        return True 
